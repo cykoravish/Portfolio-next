361c20f001e465e08db090b78aafc7c44262db98
abhinandan-verma <abhinandanverma551@gmail.com>
2024-09-12 01:51:48 +0530

    update

--- src/components/landing-hero.tsx
+++ src/components/landing-hero.tsx
@@ -39,7 +39,7 @@ export default function LandingHero() {
           <AnimatePresence>
             <FadeUp key="title-main" duration={0.6}>
               <h1 className="bg-accent bg-clip-text py-2 text-5xl font-bold text-transparent sm:text-6xl md:text-7xl xl:text-8xl">
-                Abhinandan
+                I Build Landing Pages
               </h1>
               <span className="text-xl font-semibold text-zinc-900 dark:text-zinc-100 md:text-3xl">
                 Full Stack Dev, Freelancer &amp; OpenSource Contributor
--- src/components/projects/project-card.tsx
+++ src/components/projects/project-card.tsx
@@ -25,7 +25,7 @@ export default function ProjectCard(props: ProjectCardProps) {
         type: "spring",
         duration: 0.4,
       }}
-      className="w-full overflow-hidden rounded-lg border border-accent/20 bg-background shadow-md transition-shadow duration-150 hover:shadow-md hover:shadow-accent/20 dark:bg-zinc-800 dark:hover:shadow-lg lg:max-w-[600px]"
+      className="w-full overflow-hidden rounded-lg border border-accent/20 bg-background shadow-md transition-shadow duration-150 hover:z-30 hover:shadow-xl hover:shadow-accent/20 dark:bg-zinc-800 dark:hover:shadow-lg lg:max-w-[600px]"
     >
       <Corosel images={props.imageUrl} aspectRatio={2.1} />
       <div className="p-3 text-foreground sm:p-4">
